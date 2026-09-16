87edb1a8f0d9c69ea82e76d03814cb41efd51629
juancarvajal-hub <jucarvajaln@unal.edu.co>
2026-09-16 01:40:57 -0500

    Septimo ejercicio de sql

--- Practica.sql
+++ Practica.sql
@@ -47,5 +47,9 @@ CASE
 FROM Employees
 ORDER BY employee_id ASC;
 
+--- Septimo ejercicio de leetcode con sql 
+----------------------------------------------------
 
-
+SELECT user_id, CONCAT(UPPER(SUBSTRING(name, 1, 1)), LOWER(SUBSTRING(name, 2))) AS name
+FROM Users
+ORDER BY user_id ASC;
